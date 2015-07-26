6124351479e65d3be35147b7f759fec5efb9bef8
dataumpoint <datumpoint@users.noreply.github.com>
2015-07-26 11:22:49 +1000

    Initial commit to test git repository.
    Also, first go at making changes to the supplied function stubs.
    Using the example function as a guide I have replaced the call to 'mean'
    with a call to 'cacheSolve', and changed the 'get/setmean' calls to be
    'get/setinv', and renamed the cached variable 'm' to 'inv'

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a Matrix like object that
+## caches the solution to the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+    get <- function() x
+    setinv <- function(cacheSolve) m <<- cacheSolve
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function does the work of calculating the inverse
+## of the matrix supplied.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
